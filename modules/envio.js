359c16eca29b39a8c953d688df778cb4b8af484a
castano208 <zsantiagohenao@gmail.com>
2024-05-29 12:55:10 -0500

    Se actualizado la api

--- modules/envio.js
+++ modules/envio.js
@@ -2,18 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const detalleVentaSchema = new Schema({
-  correo: { type: String, required: true },
   fechaVenta: { type: Date, required: true },
   medidaVenta: { type: String, required: true },
   descripcion: { type: String, required: true },
-  cantidad: { type: Number, required: true }
+  cantidad: { type: Number, required: true },
+  color: { type: String, required: true }
 });
 
 const envioSchema = new Schema({
   estadoEnvio: { type: String, required: true },
   totalEnvio: { type: Number, required: true },
   direccionEnvio: { type: String, required: true },
-  detalleVenta: [detalleVentaSchema] // Array de subdocumentos detalleVenta
+  correo: { type: String, required: true },
+  detalleVenta: [detalleVentaSchema]
 });
 
 const Envio = mongoose.model('Envio', envioSchema);
