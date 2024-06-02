ceb069aae8eddaa8fce6d6220b15edeb11dcd889
castano208 <zsantiagohenao@gmail.com>
2024-06-02 15:29:44 -0500

    Se actualizado la api

--- routes/envio.js
+++ routes/envio.js
@@ -6,7 +6,7 @@ const {
   enviosPut,
   enviosDelete,
   enviosCliente,
-  todoEnviosCliente,
+  EnviosTerminadosCliente,
 } = require('../controllers/envio');
 
 router.get('/envios', enviosGet);
@@ -19,6 +19,6 @@ router.delete('/envios/:id_envio', enviosDelete);
 
 router.get('/envios/:c_correo', enviosCliente);
 
-router.get('/envios/terminados/:c_correo', todoEnviosCliente);
+router.get('/envios/terminados/:c_correo', EnviosTerminadosCliente);
 
 module.exports = router;
